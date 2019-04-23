c0f19cc67dc5428745d17346e7dc91d353ef91f0
李继龙 <kljzndx@outlook.com>
2019-04-23 22:50:21 +0800

    完成LrcLine编写

---
+++ HappyStudio.Parsing.Subtitle/LRC/LrcLine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HappyStudio.Parsing.Subtitle.Interfaces;
+
+namespace HappyStudio.Parsing.Subtitle.LRC
+{
+    public class LrcLine : ISubtitleLine
+    {
+        public LrcLine(TimeSpan startTime, string content)
+        {
+            StartTime = startTime;
+            Content = content;
+        }
+
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan? EndTime { get => null; set { } }
+        public string Content { get; set; }
+
+        public override string ToString()
+        {
+            return ToString(2);
+        }
+
+        public string ToString(int msBitCount)
+        {
+            int count = msBitCount > 3 ? 3 : msBitCount;
+            if (count < 1)
+                count = 2;
+
+            int min = StartTime.Minutes + (60 * StartTime.Hours);
+            int ss = StartTime.Seconds;
+
+            string msStr = StartTime.Milliseconds.ToString().Substring(0, count);
+
+            return $"[{min:D2}:{ss:D2}.{msStr}] {Content}";
+        }
+    }
+}

5faf072329008314db973f852076965b4a977916
李继龙 <kljzndx@outlook.com>
2019-04-27 16:02:06 +0800

    添加对SrtBlock的测试

---
+++ HappyStudio.Parsing.Subtitle.Testing/SrtBlockTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using HappyStudio.Parsing.Subtitle.SRT;
+using System.IO;
+using System.Linq;
+
+namespace HappyStudio.Parsing.Subtitle.Testing
+{
+    public class SrtBlockTest
+    {
+        [Test]
+        public void Parse()
+        {
+            string content = File.ReadAllText("./test.srt");
+            SrtBlock block = new SrtBlock(content);
+            Assert.Greater(block.Lines.Count(), 10);
+        }
+    }
+}

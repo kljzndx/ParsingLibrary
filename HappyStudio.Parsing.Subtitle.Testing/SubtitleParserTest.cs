c6823390aecf84e88dd04c60495cdbc9965b59a1
李继龙 <kljzndx@outlook.com>
2019-06-11 09:06:06 +0800

    添加测试

---
+++ HappyStudio.Parsing.Subtitle.Testing/SubtitleParserTest.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace HappyStudio.Parsing.Subtitle.Testing
+{
+    public class SubtitleParserTest
+    {
+        [Test]
+        public void Parse()
+        {
+            string srtContent = File.ReadAllText("./test.srt");
+            string lrcContent = File.ReadAllText("./test.lrc");
+            var srt = SubtitleParser.Parse(srtContent);
+            var lrc = SubtitleParser.Parse(lrcContent);
+
+            Assert.True(srt != null && lrc != null);
+        }
+    }
+}

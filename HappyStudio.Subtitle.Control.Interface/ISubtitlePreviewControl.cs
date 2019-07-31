8c79463c04881329024758d732154b928567fb1e
李继龙 <kljzndx@outlook.com>
2019-07-31 16:40:25 +0800

    添加预览控件接口库

---
+++ HappyStudio.Subtitle.Control.Interface/ISubtitlePreviewControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using HappyStudio.Parsing.Subtitle.Interfaces;
+
+namespace HappyStudio.Subtitle.Control.Interface
+{
+    public interface ISubtitlePreviewControl
+    {
+        event EventHandler<ISubtitleLine> Refreshed;
+
+        void SetSubtitle(ISubtitleBlock subtitle);
+        void Refresh(TimeSpan time);
+        void Reposition(TimeSpan time);
+    }
+}

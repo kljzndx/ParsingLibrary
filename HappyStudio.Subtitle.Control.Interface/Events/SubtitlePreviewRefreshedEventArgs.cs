b85c801dfbbed38a042dde37d4259978c9b55521
李继龙 <kljzndx@outlook.com>
2019-08-22 09:54:11 +0800

    优化接口

---
+++ HappyStudio.Subtitle.Control.Interface/Events/SubtitlePreviewRefreshedEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HappyStudio.Parsing.Subtitle.Interfaces;
+
+namespace HappyStudio.Subtitle.Control.Interface.Events
+{
+    public class SubtitlePreviewRefreshedEventArgs : EventArgs
+    {
+        public SubtitlePreviewRefreshedEventArgs(ISubtitleLine oldLine, ISubtitleLine newLine)
+        {
+            OldLine = oldLine;
+            NewLine = newLine;
+        }
+
+        public ISubtitleLine OldLine { get; }
+        public ISubtitleLine NewLine { get; }
+    }
+}
--- HappyStudio.Subtitle.Control.Interface/ISubtitlePreviewControl.cs
+++ HappyStudio.Subtitle.Control.Interface/ISubtitlePreviewControl.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using HappyStudio.Parsing.Subtitle.Interfaces;
+using HappyStudio.Subtitle.Control.Interface.Events;
 
 namespace HappyStudio.Subtitle.Control.Interface
 {
     public interface ISubtitlePreviewControl
     {
-        event EventHandler<ISubtitleLine> Refreshed;
+        event EventHandler<SubtitlePreviewRefreshedEventArgs> Refreshed;
 
         void SetSubtitle(IEnumerable<ISubtitleLine> lines);
         void Refresh(TimeSpan time);

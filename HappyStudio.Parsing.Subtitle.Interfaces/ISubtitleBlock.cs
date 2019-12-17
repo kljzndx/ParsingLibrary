e53b4ec1326f198a984658a436d10402aec5aaa5
李继龙 <kljzndx@outlook.com>
2019-12-17 10:49:18 +0800

    优化接口

--- HappyStudio.Parsing.Subtitle.Interfaces/ISubtitleBlock.cs
+++ HappyStudio.Parsing.Subtitle.Interfaces/ISubtitleBlock.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace HappyStudio.Parsing.Subtitle.Interfaces
 {
     public interface ISubtitleBlock
     {
-        IEnumerable<ISubtitleLine> Lines { get; }
+        IList<ISubtitleLine> Lines { get; }
         ISubtitleBlockProperties Properties { get; }
 
         string ToString();

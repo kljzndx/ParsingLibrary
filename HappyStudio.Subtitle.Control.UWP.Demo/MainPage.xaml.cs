eaed7963bccc89c654eb5bb2f15e9ebd8ea769a6
李继龙 <kljzndx@outlook.com>
2019-12-19 11:07:38 +0800

    增加demo项目

---
+++ HappyStudio.Subtitle.Control.UWP.Demo/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using HappyStudio.Parsing.Subtitle.LRC;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace HappyStudio.Subtitle.Control.UWP.Demo
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void OpenLyricsFile_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            var picker = new FileOpenPicker();
+            picker.FileTypeFilter.Add(".lrc");
+            picker.FileTypeFilter.Add(".srt");
+            var file = await picker.PickSingleFileAsync();
+            if (file is null)
+                return;
+
+            string content = await FileIO.ReadTextAsync(file);
+            var lrcBlock = new LrcBlock(content);
+            Main_ScrollSubtitlePreview.Source = lrcBlock.Lines;
+        }
+    }
+}
---
+++ HappyStudio.Subtitle.Control.UWP.Demo/Model/DoubleToTimeSpan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace HappyStudio.Subtitle.Control.UWP.Demo.Model
+{
+    public class DoubleToTimeSpan : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            double v = (double) value;
+            TimeSpan time = TimeSpan.FromSeconds(v);
+            if (targetType == typeof(String))
+                return time.ToString();
+            return time;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
